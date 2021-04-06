555f2e37fe872d984b0c6c06a1c401f9707aac9c
XUELUOCC <3196168302@qq.com>
2021-04-06 22:13:10 +0800

    打包img ，打包html中得img（出错）--

---
+++ webpack打包img/webpack.config.js
@@ -0,0 +1,40 @@
+const pathlib=require('path');
+const htmlWebpackPlugin=require('html-webpack-plugin');
+
+module.exports={
+    entry:pathlib.resolve('./src/1.js'),
+    output:{
+        path:pathlib.resolve('build/'),
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test:/.(png|jpeg|gif)$/,
+                loader:'url-loader',
+                options:{
+                    limit:8*1024,
+                    esModule:false
+                }
+            },
+            {
+                test:/\.html$/,
+                loader:'html-loader'
+            }
+        ]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template:'./src/1.html'
+        })
+    ],
+    mode:'development'
+}
